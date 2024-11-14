9cc0f43ff1b3c0477d14853fd491a2f0678da381
Betmig Link <hola@betmig.link>
2024-11-14 15:13:27 +0100

    Added window closing detection for Linux/Windows and Mac

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -6,9 +6,21 @@ const createWindow = () => {
     height: 600
   })
 
-  win.loadFile('../renderer/index.html')
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
